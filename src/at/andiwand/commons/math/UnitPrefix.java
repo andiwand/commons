3d8dba43ab9b46c9dc65ed086c0f482a14c910a5
andiwand <stefl.andreas@gmail.com>
2012-11-28 15:23:27 +0100

    implement UnitPrefix, MetricPrefix

---
+++ src/at/andiwand/commons/math/UnitPrefix.java
@@ -0,0 +1,14 @@
+package at.andiwand.commons.math;
+
+import java.math.BigDecimal;
+
+
+public interface UnitPrefix {
+	
+	public String symbol();
+	
+	public double value();
+	
+	public BigDecimal bigValue();
+	
+}
