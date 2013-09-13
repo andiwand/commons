ebc03cf0f5f086bea2f0e9b6516b4a25364dab8b
andiwand <stefl.andreas@gmail.com>
2013-09-14 00:26:38 +0200

    refix lwxml event number

--- src/at/stefl/commons/lwxml/reader/LWXMLPushbackReader.java
+++ src/at/stefl/commons/lwxml/reader/LWXMLPushbackReader.java
@@ -21,6 +21,7 @@ public class LWXMLPushbackReader extends LWXMLFilterReader {
     private LinkedList<String> valueStack = new LinkedList<String>();
     
     private LWXMLEvent currentEvent;
+    private long eventNumberDifference;
     private String currentValue;
     private Reader valueReader;
     private CountingReader countingReader;
@@ -34,6 +35,11 @@ public class LWXMLPushbackReader extends LWXMLFilterReader {
     @Override
     public LWXMLEvent getCurrentEvent() {
         return currentEvent;
+    }
+    
+    @Override
+    public long getCurrentEventNumber() {
+        return in.getCurrentEventNumber() + eventNumberDifference;
     }
     
     @Override
@@ -67,6 +73,8 @@ public class LWXMLPushbackReader extends LWXMLFilterReader {
             } else {
                 valueReader = ClosedReader.CLOSED_READER;
             }
+            
+            eventNumberDifference++;
         }
         
         return currentEvent;
@@ -97,6 +105,8 @@ public class LWXMLPushbackReader extends LWXMLFilterReader {
     private void unreadEventImpl(LWXMLEvent event, String value) {
         eventStack.addFirst(event);
         if (event.hasValue()) valueStack.addFirst(value);
+        
+        eventNumberDifference--;
     }
     
     // TODO: implement in LWXMLReader
